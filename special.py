80eff5969032aecb7f2f9f62ae9a263c9fca8ac2
kanwartariq123-collab <kanwartariq123@gmail.com>
2026-09-04 16:38:43 +0500

    new

--- special.py
+++ special.py
@@ -2,17 +2,13 @@ import random as rd
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
 data = {
     'name': ['rabbit', 'lade', 'moti_sand'],
     'age': [100, 75, 0],
-}
-
+}                 
 df = pd.DataFrame(data)
 bars = plt.bar(df['name'], df['age'])
-
 plt.bar_label(bars, fmt='%d')
-
 plt.xlabel('overall parhai ki report')
 plt.title('saal ki report')
 plt.show()
