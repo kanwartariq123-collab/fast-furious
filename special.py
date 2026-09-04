6844dd4be3f6a2edc297f078c81fd9b662f721a6
kanwartariq123-collab <kanwartariq123@gmail.com>
2026-09-04 16:32:31 +0500

    new data

--- special.py
+++ special.py
@@ -1,3 +1,18 @@
 import random as rd
-arr =[rd.randint(1,10)for _ in range(100)]
-print(arr)
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = {
+    'name': ['rabbit', 'lade', 'moti_sand'],
+    'age': [100, 75, 0],
+}
+
+df = pd.DataFrame(data)
+bars = plt.bar(df['name'], df['age'])
+
+plt.bar_label(bars, fmt='%d')
+
+plt.xlabel('overall parhai ki report')
+plt.title('saal ki report')
+plt.show()
